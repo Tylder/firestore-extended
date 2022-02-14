0b226eeb08f43beb1ab1cc8cb09579968fd05e3d
anon <lofgrendaniel@hotmail.com>
2022-02-14 09:09:57 +0900

    added functions to emulator

--- projects/firestore-extended/src/lib/ngx/config.ts
+++ projects/firestore-extended/src/lib/ngx/config.ts
@@ -5,7 +5,7 @@ export class FirebaseConfig {
   [key: string]: any;
 }
 
-export interface FirestoreEmulatorConfig {
+export interface EmulatorConfig {
   // useEmulator: boolean;
   host: string;
   port: number;
@@ -13,12 +13,17 @@ export interface FirestoreEmulatorConfig {
 
 // export const FIREBASE_APP = new InjectionToken<FirebaseApp>('firebase_app.config');
 
-export const FIRESTORE_USE_EMULATOR: FirestoreEmulatorConfig = {
+export const FIRESTORE_USE_EMULATOR: EmulatorConfig = {
   // useEmulator: false,
   host: 'localhost',
   port: 8080,
 };
 
+export const FIREBASE_FUNCTIONS_USE_EMULATOR: EmulatorConfig = {
+  // useEmulator: false,
+  host: 'localhost',
+  port: 5001,
+};
 
 // export type FirestoreEmulatorConfig  = {
 //   useEmulator: boolean;
--- projects/firestore-extended/src/lib/ngx/ngx-firebase.service.ts
+++ projects/firestore-extended/src/lib/ngx/ngx-firebase.service.ts
@@ -1,9 +1,9 @@
 import {Inject, Injectable, Optional} from '@angular/core';
 
-import {FirebaseConfig, FIRESTORE_USE_EMULATOR, FirestoreEmulatorConfig} from './config';
+import {EmulatorConfig, FIREBASE_FUNCTIONS_USE_EMULATOR, FirebaseConfig, FIRESTORE_USE_EMULATOR} from './config';
 import {FirebaseApp, getApps, initializeApp} from 'firebase/app';
 import {connectFirestoreEmulator, Firestore, getFirestore} from 'firebase/firestore';
-
+import {connectFunctionsEmulator, Functions, getFunctions} from 'firebase/functions';
 
 @Injectable({
   providedIn: 'root'
@@ -26,7 +26,8 @@ export class NgxFirebaseService {
   public firebaseApp: FirebaseApp;
 
   constructor(@Optional() config?: FirebaseConfig,
-              @Optional() @Inject(FIRESTORE_USE_EMULATOR) public emulatorConfig?: FirestoreEmulatorConfig) {
+              @Optional() @Inject(FIRESTORE_USE_EMULATOR) public firestoreEmulatorConfig?: EmulatorConfig,
+              @Optional() @Inject(FIREBASE_FUNCTIONS_USE_EMULATOR) public functionsEmulatorConfig?: EmulatorConfig) {
 
     if (!getApps().length) {
       if (config) {
@@ -39,16 +40,25 @@ export class NgxFirebaseService {
       this.firebaseApp = getApps()[0]; // if already initialized, use that one
     }
 
-    if (emulatorConfig) {
-      connectFirestoreEmulator(getFirestore(this.firebaseApp), emulatorConfig.host, emulatorConfig.port); // v9
+    if (firestoreEmulatorConfig) {
+      connectFirestoreEmulator(getFirestore(this.firebaseApp), firestoreEmulatorConfig.host, firestoreEmulatorConfig.port); // v9
       // getFirestore(this.firebaseApp).useEmulator(emulatorConfig.host, emulatorConfig.port); // v8
     }
+    if (functionsEmulatorConfig) {
+      connectFunctionsEmulator(getFunctions(this.firebaseApp), functionsEmulatorConfig.host, functionsEmulatorConfig.port); // v9
+    }
 
+    // TODO add emulator support for remaining firebase functionality
 
   }
 
   get firestore(): Firestore {
     /** Convenience getter */
     return getFirestore(this.firebaseApp);
+  }
+
+  get functions(): Functions {
+    /** Convenience getter */
+    return getFunctions(this.firebaseApp);
   }
 }
